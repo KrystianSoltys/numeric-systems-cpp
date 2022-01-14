8c7091d312ef492b1fe6ebb94b8db4afe876aa62
KrystianSoltys <krystian.soltys7@gmail.com>
2022-01-14 19:20:12 +0100

    Logging works propertly

--- ChangingNumberSystems/log.cpp
+++ ChangingNumberSystems/log.cpp
@@ -4,12 +4,78 @@ Log::Log(const std::string& filePath)
 	: filename(filePath)
 {
 	std::ofstream file;
-	file.open(filename);
+	file.open(filename, std::ios_base::app);
 
 	if (!file.is_open())
 	{
-		throw BadFile("Problem with log file occured.");
+		throw BadLogFile("Problem with log file occured.");
 	}
 
+	file << getDate() << "   " << std::left << std::setw(13) <<
+		"Note: " << DEFAULT_WELCOME_MESS << std::endl;
+
 	file.close();
+}
+
+void Log::SetFilename(const std::string & filePath)
+{
+	if (filePath == this->filename) return;
+
+	AddToLog("Log file changed -> " + filePath);
+	filename = filePath;
+}
+
+const std::string & Log::Path() const noexcept
+{
+	return filename;
+}
+
+void Log::AddToLog(const std::string & data, bool isExc) const
+{
+	std::ofstream file;
+	file.open(filename, std::ios_base::app);
+
+	if (!file.is_open())
+	{
+		throw BadLogFile("Problem with log file occured.");
+	}
+
+	file << getDate() << "   " << std::left << std::setw(13) << 
+		((isExc) ? "Exception: " : "Note: " ) <<
+		data << std::endl;
+
+	
+	file.close();
+}
+
+std::string Log::getDate() const
+{
+	using std::to_string;
+
+	std::string day = "", month = "", year = "", hour = "", min = "", sec="";
+
+	time_t t = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+	tm l;
+	localtime_s(&l, &t);
+
+	if (l.tm_mday < 10) day = "0";
+	day += to_string(l.tm_mday);
+
+	if (l.tm_mon < 10) month = "0";
+	month += to_string(l.tm_mon+1);
+
+	year = to_string(l.tm_year + 1900);
+
+	if (l.tm_hour < 10) hour = "0";
+	hour += to_string(l.tm_hour);
+
+	if (l.tm_min < 10) min = "0";
+	min += to_string(l.tm_min);
+
+	if (l.tm_sec < 10) sec = "0";
+	sec += to_string(l.tm_sec);
+
+
+	return std::string("[" + day + "." + month + "." + year +
+						" | " + hour + ":" + min + ":" + sec + "]");
 }
--- ChangingNumberSystems/log.hpp
+++ ChangingNumberSystems/log.hpp
@@ -3,9 +3,17 @@
 
 #include <string>
 #include <fstream>
-#include "ui.hpp"
+#include <ctime>
+#include <chrono>
+#include <iomanip>
 
 static const std::string DEFAULT_LOG_FILENAME = "log.txt";
+const std::string DEFAULT_WELCOME_MESS =
+		"Program opened and ready to work.";
+const std::string DEFAULT_GOODBYE_MESS =
+		"Program closed propertly.";
+const std::string DEFAULT_CRITICAL_MESS =
+		"Program suddenly closed.";
 
 class Log
 {
@@ -13,19 +21,31 @@ public:
 	Log(const std::string& filePath = DEFAULT_LOG_FILENAME);
 	virtual ~Log() = default;
 
-	bool SetFilename(const std::string& filePath);
+	void SetFilename(const std::string& filePath);
 	const std::string& Path() const noexcept;
 	
-	bool AddToLog(const std::string& data) const;
-	bool AddToLog(const char* data) const;
+	void AddToLog(const std::string& data, bool isExc = false) const;
 
 
 protected:
 	std::string filename;
 
+	std::string getDate() const;
 };
 
+class BadLogFile : public std::exception
+{
+public:
+	BadLogFile(const char* message = "Problem with file occured!") :
+		msg_(message) {}
+	virtual const char* what() const noexcept
+	{
+		return msg_.c_str();
+	}
 
+protected:
+	std::string msg_;
+};
 
 #endif // !LOG_HPP
 
